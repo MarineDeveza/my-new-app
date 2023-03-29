a78df627d2a85343ae8b3dd9f948b695735d4246
MarineDeveza <marine.deveza@gmail.com>
2023-03-29 15:29:48 +0200

    secondcommit

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,13 @@
+//import Bulbasaur from "./assets/components/bulbasaur.png"
+
+function PokemonCard() {
+    return (<figure>
+    {<img src="./src/assets/bulbasaur.png"
+         alt="Bulbasaur"/>}
+    <figcaption>Bulbasaur</figcaption>
+</figure>)
+}
+
+export default PokemonCard;
+
+// "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
